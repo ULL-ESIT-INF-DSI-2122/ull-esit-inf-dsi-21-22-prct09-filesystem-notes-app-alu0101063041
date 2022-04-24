3c2c6764266ff1c74bb1eb1d1fecfa006bc09204
alu0101063041 <alu0101063041@ull.edu.es>
2022-04-24 17:15:56 +0000

    documentacion note y note-app

--- src/note-app.ts
+++ src/note-app.ts
@@ -1,19 +1,36 @@
 import {Note} from './note'
 import * as fs from 'fs'
 import * as chalk from 'chalk'
-
+/**
+ * Clase NoteApp contiene las funciones que hacen posible hacer las operaciones de escritura/lectura
+ * en la base de datos
+ */
 export class NoteApp {
+  /**
+   * @param database_path contiene el path de la base de datos donde se guardaran las notas de los diferentes usuarios
+   */
   private database_path: string = '/home/usuario/practicas/ull-esit-inf-dsi-21-22-prct09-filesystem-notes-app-alu0101063041/database'
   constructor() {}
+  /**
+   * Obtenemos el path de un usuario en concreto
+   * @param user indica donde se encuentra el directorio del usuario
+   * @returns Devuelve un string con el path completo del usuario
+   */
   getPathUser(user: string): string {
     return this.database_path + '/' + user
   }
+  /**
+   * Obtenemos el path de un fichero .json
+   * @param user indica el directorio del usuario
+   * @param title indica el fichero
+   * @returns devuelve el path completo de un usuario y un fichero especifico
+   */
   getFilePath(user: string, title: string): string {
     return this.getPathUser(user) + '/' + title + '.json'
   }
   /**
    * añade un directorio para un usuario
-   * @param user
+   * @param user nombre del directorio que vamos a crear
    */
   createNewDir(user: string) {
     if (!this.checkDir(user)) {
@@ -23,12 +40,31 @@ export class NoteApp {
       // console.log(chalk.white(`${user} ya tiene un directorio`))
     }
   }
+  /**
+   * Comprueba la existencia de un directorio
+   * @param user directorio a buscar
+   * @returns true o false
+   */
   checkDir(user: string): boolean {
     return fs.existsSync(this.getPathUser(user))
   }
+  /**
+   * Comprueba a existencia de un archivo .json en un directorio especifico
+   * @param user indica el directorio
+   * @param title indica el fichero
+   * @returns true o false
+   */
   checkFile(user: string, title: string): boolean {
     return fs.existsSync(this.getFilePath(user, title))
   }
+  /**
+   * Añade una nota a la base de datos
+   * @param user usuario/directorio donde se fguarda la nota
+   * @param title nombre del fichero y titulo de la nota con el que se guardara en la base de datos
+   * @param body Cuerpo de la nota
+   * @param color color que le asignaremos
+   * @returns true o false
+   */
   addNote(user: string, title: string, body: string, color: string): boolean {
     this.createNewDir(user)
     const newNote: Note = new Note(title, body, color)
@@ -41,6 +77,12 @@ export class NoteApp {
       return false
     }
   }
+  /**
+   * Elimina una nota de un usuario y titulo especifico
+   * @param user usuario/directorio al que le pertenece la nota
+   * @param title nombre con el que esta guardada la nota
+   * @returns true o false
+   */
   removeNote(user: string, title: string): boolean {
     if (!this.checkFile(user, title)) {
       console.log(chalk.red(`La nota con titulo ${title} no esta en la base de datos`))
@@ -51,10 +93,21 @@ export class NoteApp {
       return true
     }
   }
+  /**
+   * Obtenemos una nota de la base de datos a partir de un usuario y un titulo
+   * @param user usuario/directorio al que le pertenece la nota
+   * @param title nombre con el que esta guardada la nota
+   * @returns true o false
+   */
   getNote(user:string, title:string) {
     const note = JSON.parse(fs.readFileSync(`${this.getPathUser(user)}/${title}.json`, 'utf-8'))
     return note
   }
+  /**
+   * Obtenemos todas las notas de un usuario
+   * @param user usuario/directorio al que le pertenece la nota
+   * @returns un array de objetos Note
+   */
   getNotesUser(user:string):Note[] {
     const titles = fs.readdirSync(this.getPathUser(user))
     const notes: Note[] = []
@@ -65,21 +118,33 @@ export class NoteApp {
     })
     return notes
   }
-
-  listNotes(user:string) {
+  /**
+   * Muestra el titulo de las notas que pertenece a un usuario
+   * @param user usuario/directorio de las notas que queremos listar
+   */
+  listNotes(user:string):boolean {
     if (this.checkDir(user)) {
       if (this.getNotesUser(user).length === 0) {
         console.log(chalk.red(`${user} no tiene notas guardadas`))
+        return false
       } else {
         console.log(chalk.green(`Estas son las notas de ${user}`))
         for (const note of this.getNotesUser(user)) {
           note.printTitle()
         }
+        return true
       }
     } else {
       console.log(chalk.red(`el usuario: ${user} no esta en la base de datos`))
+      return false
     }
   }
+  /**
+   * Muestra una nota especifica de un usuario y un titulo
+   * @param user usuario/directorio de la nota
+   * @param title titulo de la nota
+   * @returns true o false
+   */
   showNote(user:string, title: string): boolean {
     if (this.checkDir(user)) {
       if (this.checkFile(user, title)) {
@@ -97,6 +162,13 @@ export class NoteApp {
       return false
     }
   }
+  /**
+   * Edita el contenido de una nota de un usuario
+   * @param user usario/directorio donde se encuentra la nota
+   * @param title titulo de la nota
+   * @param body contenido de la nota que se va a actualizar
+   * @returns true o false
+   */
   editNote(user: string, title: string, body: string): boolean {
     if (this.checkDir(user)) {
       if (this.checkFile(user, title)) {
--- src/note.ts
+++ src/note.ts
@@ -4,30 +4,66 @@ import * as chalk from 'chalk'
  * Clase Nota que representa las notas que un usuario puede almacenar
  */
 export class Note {
+  /**
+   * @param title titulo de la nota
+   * @param body contenido de la nota
+   * @param color color de la nota
+   */
   constructor(private title: string, private body:string, private color: string) {}
-  public getTitle(): string {
+  /**
+   *
+   * @returns Devuelve el titulo de la nota
+   */
+  getTitle(): string {
     return this.title;
   }
-  public getBody(): string {
+  /**
+   *
+   * @returns Devuelve el contenido de la nota
+   */
+  getBody(): string {
     return this.body;
   }
-  public getColor(): string {
+  /**
+   *
+   * @returns Devuelve el color correspondiente a la nota
+   */
+  getColor(): string {
     return this.color;
   }
-  public setTitle(title: string) {
+  /**
+   * Cambia el titulo de la nota
+   * @param title
+   */
+
+  setTitle(title: string) {
     this.title = title;
   }
-  public setBody(body: string) {
+  /**
+   * Cambia el contenido de la nota
+   * @param body
+   */
+  setBody(body: string) {
     this.body = body;
   }
-  public setColor(color: string) {
+  /**
+   * Cambia el color de la nota
+   * @param color
+   */
+  setColor(color: string) {
     this.color = color;
   }
-  public noteToJson(): string {
+  /**
+   *
+   * @returns Devuelve un string con formato JSON
+   */
+  noteToJson(): string {
     return JSON.stringify({title: this.getTitle(), body: this.getBody(), color: this.getColor()})
   }
-
-  public printTitle(): void {
+  /**
+   * Muestra el titulo de la nota con el color correspondiente por consola
+   */
+  printTitle(): void {
     switch (this.color) {
       case 'red':
         console.log(chalk.red(this.getTitle()))
@@ -49,7 +85,10 @@ export class Note {
         break
     }
   }
-  public printBody(): void {
+  /**
+   * Muestra el contenido de la nota con su color correspondiente
+   */
+  printBody(): void {
     switch (this.getColor()) {
       case 'red':
         console.log(chalk.red(this.getBody()))
